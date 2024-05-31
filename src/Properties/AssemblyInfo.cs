eb6608f9303d28079462dcfe6b1d9e78728c6919
Dennis van Gils <dennisvg111@gmail.com>
2024-05-31 16:33:00 +0200

    Add project structure

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("354dcb2f-5995-4bd9-bb0a-24b3dfdee855")]
+[assembly: InternalsVisibleTo("DG.Epub.Tests")]
---
+++ tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f959151-a91f-4553-946f-669fde934a8d")]
