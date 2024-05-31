3d68bce1564ad8debb15f1a58418746b4ec4e0af
Dennis van Gils <dennisvg111@gmail.com>
2024-05-31 17:01:09 +0200

    Add initial EpubBook class stub

---
+++ src/EpubBook.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace DG.Epub
+{
+    public class EpubBook
+    {
+
+        public static EpubBook FromStream(Stream s)
+        {
+            using (ZipArchive zip = new ZipArchive(s))
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ tests/EpubBookTests.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Xunit;
+
+namespace DG.Epub.Tests
+{
+    public class EpubBookTests
+    {
+        [Fact]
+        public void FromStream_Tests()
+        {
+            using (var exampleStream = File.OpenRead("minimal.epub"))
+            {
+                var book = EpubBook.FromStream(exampleStream);
+            }
+        }
+    }
+}
