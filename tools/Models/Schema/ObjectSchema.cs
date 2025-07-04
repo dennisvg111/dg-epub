4b2142b4c15defcae91d343569e0080e112e82f8
dennisvg111 <dennisvg111@gmail.com>
2025-07-04 18:56:21 +0200

    Add xml attributes to class

--- tools/Models/Schema/ObjectSchema.cs
+++ tools/Models/Schema/ObjectSchema.cs
@@ -1,7 +1,18 @@
-﻿namespace DG.Epub.CodeGeneration.Models.Schema;
+﻿using YamlDotNet.Serialization;
+
+namespace DG.Epub.CodeGeneration.Models.Schema;
 
 public class ObjectSchema : BaseSchema
 {
+    [YamlMember(Alias = "xml_type", ApplyNamingConventions = false)]
+    public string XmlType { get; set; }
+
+    [YamlMember(Alias = "xml_root", ApplyNamingConventions = false)]
+    public string XmlRoot { get; set; }
+
+    [YamlMember(Alias = "xml_namespace", ApplyNamingConventions = false)]
+    public string XmlNamespace { get; set; }
+
     public Dictionary<string, BaseSchema> Properties { get; set; }
     public Dictionary<string, BaseSchema> Attributes { get; set; }
 
